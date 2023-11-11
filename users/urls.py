81218ee09d2264a8533775f40567d2e903d7ed22
Wali-Muhammad123 <renedescartian@gmail.com>
2023-11-11 17:51:29 +0500

    Updated URLs.py

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('/rating', UserRatingView.as_view(), name='user_rating'),
-    path('update/profile/customer', update_customer_profile, name='update_talent_profile'),
-    path('update/profile/rider', update_customer_profile, name='update_talent_profile'),
-    path('get/profile/customer', update_customer_profile, name='update_talent_profile'),
-    path('get/profile/rider', update_customer_profile, name='update_talent_profile'),
-    path('update/availability/rider', update_rider_location_status, name='update_talent_profile'),
-    path('update/availability/customer', update_customer_location_status, name='update_talent_profile'),
+    path('/update/profile/customer', update_customer_profile, name='update_talent_profile'),
+    path('/update/profile/rider', update_customer_profile, name='update_talent_profile'),
+    path('/get/profile/customer', update_customer_profile, name='update_talent_profile'),
+    path('/get/profile/rider', update_customer_profile, name='update_talent_profile'),
+    path('/update/availability/rider', update_rider_location_status, name='update_talent_profile'),
+    path('/update/availability/customer', update_customer_location_status, name='update_talent_profile'),
 ]
