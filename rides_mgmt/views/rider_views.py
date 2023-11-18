2f52d9bddc28c11679e1ef621a014c0240b39ccd
mimran34 <mimranhaider34@gmail.com>
2023-11-19 00:48:08 +0500

    Added Model Viewset property

--- rides_mgmt/views/rider_views.py
+++ rides_mgmt/views/rider_views.py
@@ -1,15 +1,24 @@
 from dj_rest_auth.jwt_auth import JWTCookieAuthentication
-from rest_framework import viewsets
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from users.permissions import IsRiderUser
+from ..models import RideObject
 from ..serializers.find_customers import FindCustomerCardSerializer
 from ..utils.db import find_customers
 
 
-class FindCustomersViewSet(viewsets.ViewSet):
+class FindCustomersViewSet(viewsets.ModelViewSet):
     serializer_class = FindCustomerCardSerializer
     authentication_classes = [JWTCookieAuthentication]
     permission_classes = [IsRiderUser]
 
     def get_queryset(self):
-        return find_customers(self.request.user.rider)
+        if self.action == 'list':
+            return find_customers(rider=self.request.user.rider)
+        elif self.action == 'retrieve':
+            return RideObject.objects.filter(id=self.kwargs.get('pk'), rider=self.request.user.rider)
+        else:
+            return RideObject.objects.none()
+
