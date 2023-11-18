856728215d0a569d11b7ccfe04ea1eeef3cdff38
mimran34 <mimranhaider34@gmail.com>
2023-11-18 05:17:56 +0500

    Updated urls.py

--- rides_mgmt/urls.py
+++ rides_mgmt/urls.py
@@ -3,11 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'available_riders', ListRidersViewSet, basename="available_riders")
+router.register(r'', ListRidersViewSet, basename="available_riders")
 find_customers_router = DefaultRouter()
-find_customers_router.register(r'find_customers', FindCustomersViewSet, basename="find_customers")
+find_customers_router.register(r'', FindCustomersViewSet, basename="find_customers")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(find_customers_router.urls)),
+    path('available_riders/', include(router.urls)),
+    path('find_customers/', include(find_customers_router.urls)),
 ]
